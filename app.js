72354d5488f438328d01f87e40f17e2bad322f96
Layton0-0 <yeshan0224@naver.com>
2021-10-25 21:03:14 +0900

    그리기 성공

---
+++ app.js
@@ -0,0 +1,39 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+let painting = false;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+function stopPainting() {
+  painting = false;
+  ctx.closePath();
+}
+
+function startPainting() {
+  painting = true;
+  ctx.beginPath();
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
