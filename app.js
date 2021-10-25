e525a722c09fb79868728d2d297984f5f3dc0d9b
Layton0-0 <yeshan0224@naver.com>
2021-10-25 21:51:31 +0900

    브러쉬 수정

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("controls__colors");
+const range = document.getElementById("jsRange");
+const button = document.getElementById("jsMode");
+
+canvas.width = 550;
+canvas.height = 550;
 
 let painting = false;
+let filling = false;
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 function stopPainting() {
   painting = false;
-  ctx.closePath();
 }
 
 function startPainting() {
   painting = true;
-  ctx.beginPath();
 }
 
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
+    ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
     ctx.lineTo(x, y);
@@ -27,8 +33,24 @@ function onMouseMove(event) {
   }
 }
 
-function onMouseDown(event) {
-  painting = true;
+function handleRangeChange() {
+  const size = range.value;
+  ctx.lineWidth = size;
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
+function handleModeClick() {
+  if (filling == true) {
+    filling = false;
+    button.innerText = "fill";
+  } else {
+    filling = true;
+    button.innerText = "paint";
+  }
 }
 
 if (canvas) {
@@ -36,4 +58,16 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+}
+
+if (colors) {
+  Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+}
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (button) {
+  button.addEventListener("click", handleModeClick);
 }
